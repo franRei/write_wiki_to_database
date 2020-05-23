11cc8a9036552efbcc730764312347f5e467f0a3
franRei <38581197+franRei@users.noreply.github.com>
2020-05-23 12:10:56 +0200

    Update Database_Writer.java

--- src/Database_Writer.java
+++ src/Database_Writer.java
@@ -5,44 +5,34 @@ import java.sql.*;
 public class Database_Writer {
 
     public static void open_Database(String word, String description) {
-        String user = "franzi";
-        String pw = "password";
+        String user = "user_name_here";
+        String pw = "password_here";
         int synEx = 0;
+        //sql table name here
         String tablename = "wiki_db_13_01";
-        //description = description.replaceAll("\\{", "\\\\{");
-        //description = description.replaceAll("\\}", "\\\\}");
-        //description = description.replaceAll("\\|", "\\\\|");
-        //description = description.replaceAll("\\[", "\\\\[").replaceAll("\\]", "\\\\]").replaceAll("\\:", "\\\\:");
+       
         description = description.replaceAll("'","''");
-        //description = "/*" + description + "*/";
+       
         word = word.replaceAll("'","''");
-//        System.out.println(description);
 
         String esquel = "INSERT INTO " + tablename + " ( wiki_word, wiki_description ) "
                 + " VALUES ( '" + word +  "','" + description + "');";
-//CREATE DATABASE proto CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-//CREATE TABLE wiki_db_29_12 (wiki_word VARCHAR(150), wiki_description TEXT);
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/proto?useUnicode=true&characterEncoding=UTF-8&useSSL=false&" +
                     "user=" + user + "&password=" + pw);
-            //ResultSet rs = null;
 
             PreparedStatement stmt = conn.prepareStatement(esquel, Statement.RETURN_GENERATED_KEYS);
             stmt.executeUpdate();
             conn.close();
         }
         catch (ClassNotFoundException ex) {System.err.println(ex.getMessage());}
-       // catch (IllegalAccessException ex) {System.err.println(ex.getMessage());}
-        //catch (InstantiationException ex) {System.err.println(ex.getMessage());}
         catch (MySQLSyntaxErrorException e) {
             System.out.println("SyntaxException: " + synEx++);
         }
         catch (SQLException ex)           {System.err.println(ex.getMessage());}
         finally {
-
         }
     }
-
 }
